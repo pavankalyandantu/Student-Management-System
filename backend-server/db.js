ced2649c2748f8a35bbd04d18b638ab3edb17f5e
Pavankalyan Dantu <pavankalyandantu@gmail.com>
2026-07-17 21:13:49 +0530

    Prepare backend for deployment

--- backend-server/db.js
+++ backend-server/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-  password: process.env.DB_PASSWORD,
-    database: "student_management"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
